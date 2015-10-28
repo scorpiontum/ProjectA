a8cf91cc4d019089927a3de3002e9707deecf9ca
arunwongdotw <arunwongdotw@gmail.com>
2015-10-28 19:54:17 +0700

    สร้างรับค่าชื่อ เงินเดือน ยอดขาย

--- src/ProjectA.java
+++ src/ProjectA.java
@@ -10,6 +10,13 @@ public class ProjectA {
 		double Commission;
 		double compensation;
 		
+		Scanner in = new Scanner(System.in);
+		System.out.print("Input your name : ");
+		name = in.nextLine();						//รับค่าชื่อ
+		System.out.print("Input your salary : ");
+		salary = in.nextDouble();					//รับค่าเงินเดือน
+		System.out.print("Input your sales : ");
+		sales = in.nextDouble();
 		
 	}
 
