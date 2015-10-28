551cc21a6e2849811d2a2e246443e4277da93606
tumscorpion <prateep2007@gmail.com>
2015-10-28 19:51:32 +0700

    สร้าง Class ProjectA import library ประกาศตัวแปร

---
+++ src/ProjectA.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class ProjectA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String name;
+		double salary;
+		double sales;
+		double Commission;
+		double compensation;
+		
+		
+	}
+
+}
